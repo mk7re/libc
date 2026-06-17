8f2a88dd5a2016f2ed799bfcc1a16eec6b1f7c88
Bsquo <suyikaki37@gmail.com>
2026-06-17 12:58:27 +0100

    Add mem.h

---
+++ include/mem.h
@@ -0,0 +1,14 @@
+#ifndef MEM_H
+#define MEM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __rt_memclr_w(void * ptr, unsigned int size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MEM_H */
